0999c7e96ef1dea5b2248503f574c9acc3b55f16
juhana-peltomaa <juhana.peltomaa@helsinki.fi>
2021-06-10 13:37:53 +0200

    Exercise 3.3

--- index.js
+++ index.js
@@ -27,14 +27,24 @@ let persons = [
     ]
 
 app.get('/info', (request, response) => {
-
     response.send(`<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`)
   })
   
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
